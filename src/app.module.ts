49014ee08cf24f4e7ef60c0c49eeac8263918d29
Ibrahim Abubakar <ikadismile@gmail.com>
2022-02-12 18:18:05 +0000

    added env, config, and graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import {join} from 'path'
+import { UserModel } from './user/model/user.model';
+
+
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot(),
+    UserModel,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
